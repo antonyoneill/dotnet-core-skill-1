7e823fd8072919ae69af44f54a2c7debcd12929e
Antony O'Neill <antony@ant-web.co.uk>
2020-11-06 18:12:09 +0000

    Refactor out I and V
    Forgive me for my sins

--- ApplicationLibrary/RomanNumerals.cs
+++ ApplicationLibrary/RomanNumerals.cs
@@ -4,23 +4,26 @@ namespace ApplicationLibrary
 {
     public static class RomanNumerals
     {
+        private static string V = "V";
+        private static string I = "I";
+        
         public static string NumberToNumeral(int number)
         {
             if (number > 5)
             {
-                return "V" + "".PadLeft(number - 5, 'I');
+                return V + "".PadLeft(number - 5, I.ToCharArray()[0]);
             }
             if (number == 5)
             {
-                return "V";
+                return V;
             }
 
             if (number == 4)
             {
-                return "IV";
+                return I + V;
             }
 
-            return "".PadLeft(number, 'I');
+            return "".PadLeft(number, I.ToCharArray()[0]);
         }
     }
 }
